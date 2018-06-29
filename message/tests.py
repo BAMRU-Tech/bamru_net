b4a46a78e2734850272ca86600d951b3379ee843
Kevin Duncklee <github.kevin@duncklee.net>
2018-06-29 02:11:30 +0000

    Add first message test for email

--- message/tests.py
+++ message/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.core import mail
+from django.test import Client, TestCase
+from django.test.utils import override_settings
+from model_mommy import mommy
 
-# Create your tests here.
+from bnet.models import Email
+
+from .models import *
+
+
+@override_settings(EMAIL_BACKEND='anymail.backends.test.EmailBackend')
+class OutgoingEmailTestCase(TestCase):
+    def setUp(self):
+        self.address = 'message_test@example.com'
+        self.email = mommy.make(Email,
+                                address=self.address,
+                                make_m2m=True)
+        self.distribution = mommy.make(Distribution,
+                                       member=self.email.member,
+                                       email=True,
+                                       phone=False,
+                                       make_m2m=True)
+
+    def test_send(self):
+        self.distribution.message.send()
+
+        # Test that one message was sent:
+        self.assertEqual(len(mail.outbox), 1)
+
+        self.assertEqual(mail.outbox[0].to, [self.address])
