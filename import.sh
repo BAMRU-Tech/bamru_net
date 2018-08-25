010a70735cb8f4a71edee135fd15dfd7fb428233
Kevin Duncklee <github.kevin@duncklee.net>
2018-08-25 04:42:20 +0000

    Add import script

---
+++ import.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'drop database bnet_development;' | psql -U postgres
+echo 'create database bnet_development;' | psql -U postgres
+psql -U postgres bnet_development < ~/bnet.sql
+rm */migrations/00*
+./manage.py makemigrations
+./manage.py migrate
+psql -U postgres bnet_development < import.sql
+echo "update main_member set is_staff=TRUE, is_superuser=TRUE where id in (367, 559);" | python manage.py dbshell
+python manage.py sqlsequencereset main | python manage.py dbshell
+python manage.py sqlsequencereset message | python manage.py dbshell
