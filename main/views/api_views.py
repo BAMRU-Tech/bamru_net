b00668de78e88483fbb5d9a4a3e2f3df6d557db7
Kevin Duncklee <github.kevin@duncklee.net>
2018-08-30 03:57:19 +0000

    Add start time range filter for Event

--- main/views/api_views.py
+++ main/views/api_views.py
@@ -2,6 +2,7 @@ from main.models import *
 from main.serializers import *
 
 from rest_framework import generics, permissions, viewsets
+from django_filters import rest_framework as filters
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
@@ -10,10 +11,18 @@ class MemberViewSet(viewsets.ModelViewSet):
     filter_fields = ('member_rank', )
     search_fields = ('username',  )
 
+
+class EventFilter(filters.FilterSet):
+    start = filters.DateFromToRangeFilter()
+    class Meta:
+        model = Event
+        fields = ('type', 'start', )
+
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all().order_by('-start')
     permission_classes = (permissions.IsAuthenticated,)
-    filter_fields = ('type', 'location', )
+    filterset_class = EventFilter
     search_fields = ('title', 'description', 'location', )
     def get_serializer_class(self):
         if getattr(self, 'action', None) == 'list':
