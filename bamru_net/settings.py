d832df17d1c998f8327b2d1d234e9a54f5d91197
Kevin Duncklee <github.kevin@duncklee.net>
2018-09-12 00:50:46 +0000

    Add SSL support

--- bamru_net/settings.py
+++ bamru_net/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOST'], 'localhost', ]
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOST'].split(',') + ['localhost', ]
 
 
 # Application definition
