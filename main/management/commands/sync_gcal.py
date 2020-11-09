5ee44fb00aeeb95b14edb5134829149b760c90f5
Kevin Duncklee <github.kevin@duncklee.net>
2020-11-09 05:02:09 +0000

    Update gcal management commands. #531

--- main/management/commands/sync_gcal.py
+++ main/management/commands/sync_gcal.py
@@ -8,7 +8,13 @@ class Command(BaseCommand):
         group = parser.add_mutually_exclusive_group(required=True)
         group.add_argument('--event', type=int, help="Id of event to sync")
         group.add_argument('--all', action='store_true')
-        group.add_argument('--clear', action='store_true')
+        group.add_argument('--all_public', action='store_true')
+        group.add_argument('--all_private', action='store_true')
+
+        group_all = parser.add_mutually_exclusive_group(required=True)
+        group_all.add_argument('--clear', action='store_true')
+        group_all.add_argument('--delete', action='store_true')
+        group_all.add_argument('--add', action='store_true')
 
     def handle(self, *args, **options):
         gcal_manager = get_gcal_manager(fallback_manager=None)
@@ -16,13 +22,25 @@ class Command(BaseCommand):
             print("google calendar sync not configured")
             return
 
-        if options['all']:
+        if options['all'] or options['all_public']:
             if options['clear']:
-                gcal_manager.clear()
+                gcal_manager.clear_public()
+            elif options['delete']:
+                gcal_manager.delete_public()
+            elif options['add']:
+                gcal_manager.sync_public(Event.objects.all())
             else:
-                gcal_manager.delete_all()
-            gcal_manager.sync_all(Event.objects.all())
-        elif options['event']:
+                print('Specify --clear, --delete, or --add.')
+        if options['all'] or options['all_private']:
+            if options['clear']:
+                gcal_manager.clear_private()
+            elif options['delete']:
+                gcal_manager.delete_private()
+            elif options['add']:
+                gcal_manager.sync_private(Event.objects.all())
+            else:
+                print('Specify --clear, --delete, or --add.')
+        if options['event']:
             event = Event.objects.get(id=options['event'])
             print(event.title, event.start_at, event.finish_at)
             print(event.gcal_id)
