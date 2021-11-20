742487bf43a809c1fdd0e07309d93ecf348c79a1
Kevin Duncklee <github.kevin@duncklee.net>
2021-11-20 01:27:01 +0000

    Add redis setup and dev slot.

--- deploy/azure_setup.sh
+++ deploy/azure_setup.sh
@@ -8,6 +8,7 @@ POSTGRES_NAME=${NAME}-db
 SERVICEPLAN_NAME=${NAME}-sp
 WEBAPP_NAME=${NAME}-app
 STORAGE_NAME=${NAME}
+REDIS_NAME=${NAME}-redis
 SLOT=staging
 HOSTNAME=$2
 
@@ -22,6 +23,7 @@ POSTGRES_SKU=B_Gen5_1
 RUNTIME="python:3.6"
 
 : "${SETUP_AZ:=false}"
+: "${SETUP_CERT:=false}"
 : "${SETUP_STORE:=false}"
 : "${SETUP_DB:=false}"
 : "${SETUP_CONFIG:=false}"
@@ -39,23 +41,35 @@ if $SETUP_AZ; then
 
   az postgres up --resource-group $RESOURCEGROUP_NAME --location $LOCATION --sku-name $POSTGRES_SKU --server-name $POSTGRES_NAME --database-name $POSTGRES_DB --admin-user $POSTGRES_USER --admin-password $POSTGRES_PASSWORD --ssl-enforcement Enabled
 
+  az redis create --name $REDIS_NAME --resource-group $RESOURCEGROUP_NAME --location $LOCATION --sku Basic --vm-size C0
+
   az appservice plan create --name $SERVICEPLAN_NAME --resource-group $RESOURCEGROUP_NAME --sku $SERVICEPLAN_SKU --is-linux
 
   az webapp create --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --plan $SERVICEPLAN_NAME --runtime $RUNTIME
 
   az webapp update -g $RESOURCEGROUP_NAME -n $WEBAPP_NAME --https-only true
-  az webapp config set --ftps-state Disabled --resource-group $RESOURCEGROUP_NAME -n $WEBAPP_NAME
+  az webapp config set --ftps-state Disabled --resource-group $RESOURCEGROUP_NAME -n $WEBAPP_NAME --startup-file "deploy/azure_startup.sh"
+  az webapp log config -g $RESOURCEGROUP_NAME -n $WEBAPP_NAME --docker-container-logging filesystem --application-logging filesystem
+
 
   # az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n $WEBAPP_NAME --settings #Name=Value
-  az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n $WEBAPP_NAME --settings @azure_settings.json
+  # az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n $WEBAPP_NAME --settings @azure_settings.json
 
   az webapp deployment slot create --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --slot $SLOT --configuration-source $WEBAPP_NAME
   az webapp update -g $RESOURCEGROUP_NAME -n $WEBAPP_NAME --https-only true -s $SLOT
 
+  # Create a dev slot with git local deploy
+  az webapp deployment slot create --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --slot dev --configuration-source $WEBAPP_NAME
+  az webapp update -g $RESOURCEGROUP_NAME -n $WEBAPP_NAME --https-only true -s dev
+  az webapp deployment source config-local-git --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --slot dev
+fi
+
+if $SETUP_CERT; then
   # Create and bind a free certificate
-  #az webapp config hostname add --webapp-name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --hostname $HOSTNAME
-  #thumbprint=$(az webapp config ssl create  --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --hostname $HOSTNAME  --output tsv --query thumbprint)
-  #az webapp config ssl bind --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --certificate-thumbprint $thumbprint --ssl-type SNI
+  az webapp config hostname add --webapp-name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --hostname $HOSTNAME
+  thumbprint=$(az webapp config ssl create  --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --hostname $HOSTNAME  --output tsv --query thumbprint)
+  echo "If the thumbprint is not ready, wait a few minutes and re-try."
+  az webapp config ssl bind --name $WEBAPP_NAME --resource-group $RESOURCEGROUP_NAME --certificate-thumbprint $thumbprint --ssl-type SNI
 fi
 
 if $SETUP_STORE; then
@@ -72,6 +86,7 @@ if $SETUP_STORE; then
   az storage container create --name media --account-name $STORAGE_NAME --resource-group $RESOURCEGROUP_NAME --public-access off
   az storage container create --name static --account-name $STORAGE_NAME --resource-group $RESOURCEGROUP_NAME --public-access container
   az storage cors add --methods GET --service b --origins "https://${WEBAPP_NAME}-${SLOT}.azurewebsites.net" --account-name $STORAGE_NAME
+  az storage cors add --methods GET --service b --origins "https://${WEBAPP_NAME}-dev.azurewebsites.net" --account-name $STORAGE_NAME
   az storage cors add --methods GET --service b --origins "https://${WEBAPP_NAME}.azurewebsites.net" --account-name $STORAGE_NAME
   az storage cors add --methods GET --service b --origins "https://${HOSTNAME}" --account-name $STORAGE_NAME
 fi
@@ -86,28 +101,33 @@ fi
 
 
 AZURE_STORAGE_KEY=$(az storage account keys list  --account-name $STORAGE_NAME --resource-group $RESOURCEGROUP_NAME --query [0].value -o tsv)
-
+AZURE_REDIS_KEY=$(az redis list-keys --name $REDIS_NAME --resource-group $RESOURCEGROUP_NAME --query primaryKey -o tsv)
 
 if $SETUP_CONFIG; then
   sed -e "s#X_AZURE_STORAGE_KEY#$AZURE_STORAGE_KEY#g" \
       -e "s/X_AZURE_STORAGE_ACCOUNT_NAME/$STORAGE_NAME/g" \
+      -e "s#X_CELERY_BROKER_URL#rediss://:${AZURE_REDIS_KEY}@${REDIS_NAME}.redis.cache.windows.net:6380/0#g" \
       -e "s/X_HOSTNAME/${WEBAPP_NAME}-${SLOT}.azurewebsites.net/g" \
       -e "s/X_POSTGRES_HOST/${POSTGRES_NAME}.postgres.database.azure.com/g" \
       -e "s/X_POSTGRES_DB/$POSTGRES_DB/g" \
       -e "s/X_POSTGRES_PASS/$POSTGRES_PASSWORD/g " \
       -e "s/X_POSTGRES_USER/${POSTGRES_USER}@${POSTGRES_NAME}/g" \
-      azure_settings.json > processed.json
+      azure_settings.json > processed-${SLOT}.json
 
-  az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n ${WEBAPP_NAME} --slot ${SLOT} --settings @processed.json
+  sed -e "s/${WEBAPP_NAME}-${SLOT}/${WEBAPP_NAME}/g" processed-${SLOT}.json > processed.json
+  sed -e "s/${WEBAPP_NAME}-${SLOT}/${WEBAPP_NAME}-dev/g" processed-${SLOT}.json > processed-dev.json
 
-  sed -i -e "s/${WEBAPP_NAME}-${SLOT}/${WEBAPP_NAME}/g" processed.json
+  az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n ${WEBAPP_NAME} --slot ${SLOT} --settings @processed-${SLOT}.json @azure_secrets.json
+  az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n ${WEBAPP_NAME}  --slot dev --settings @processed-dev.json @azure_secrets.json
+  az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n ${WEBAPP_NAME} --settings @processed-dev.json @azure_secrets.json
 
-  az webapp config appsettings set --resource-group $RESOURCEGROUP_NAME -n ${WEBAPP_NAME} --settings @processed.json
-
-  rm processed.json
+  # rm processed.json processed-${SLOT}.json processed-dev.json
 fi
 
 #az webapp deployment github-actions add --repo "kduncklee/bamru_net_azure" --branch azure --resource-group $RESOURCEGROUP_NAME -n ${WEBAPP_NAME} --slot ${SLOT}
 
 echo Storage command:
 echo az storage blob upload-batch -d media -s . --account-name $STORAGE_NAME --account-key \'$AZURE_STORAGE_KEY\'
+
+echo DB Backup Command:
+echo pg_dump -Fc -v \"host=${POSTGRES_NAME}.postgres.database.azure.com port=5432 user=${POSTGRES_USER}@${POSTGRES_NAME} password=${POSTGRES_PASSWORD} dbname=$POSTGRES_DB sslmode=require\" -f backup.dump
