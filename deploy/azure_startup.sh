c6b49378f67a8eddf8d8625b43ccbe5247621886
Kevin Duncklee <github.kevin@duncklee.net>
2022-02-21 21:52:13 +0000

    Use 4 gunicorn workers and reduce timeout to 30s

--- deploy/azure_startup.sh
+++ deploy/azure_startup.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 
 python manage.py qcluster &
 
-gunicorn --timeout 600 --access-logfile '-' --error-logfile '-' --bind=0.0.0.0:8000 --chdir=. bamru_net.wsgi
+gunicorn --workers 4 --threads 1 --timeout 30 --access-logfile '-' --error-logfile '-' --bind=0.0.0.0:8000 --chdir=. bamru_net.wsgi
