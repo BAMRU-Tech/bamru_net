40ed87763151ef70704349d1bf14191416924dae
Josiah Boning <josiah.boning@bamru.org>
2022-02-21 12:00:14 -0800

    Fix O(n) queries in photo gallery view

--- main/views/member_views.py
+++ main/views/member_views.py
@@ -51,6 +51,11 @@ class MemberDetailView(LoginRequiredMixin, generic.DetailView):
 class MemberPhotoGalleryView(MemberListView):
     template_name = 'member_photo_gallery.html'
 
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related(
+            'memberphoto_set',
+        )
+
 
 class MemberEditView(PermissionRequiredMixin, generic.base.TemplateView):
     template_name = 'member_edit.html'
