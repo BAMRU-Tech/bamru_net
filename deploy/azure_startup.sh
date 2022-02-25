62a2e8977291c3f9076e74a307898ba65d8a89e7
Josiah Boning <josiah.boning@bamru.org>
2022-02-25 11:35:14 -0800

    Send gunicorn logs to files

--- deploy/azure_startup.sh
+++ deploy/azure_startup.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 
 python manage.py qcluster &
 
-gunicorn --workers 4 --threads 1 --timeout 30 --access-logfile '-' --error-logfile '-' --bind=0.0.0.0:8000 --chdir=. bamru_net.wsgi
+gunicorn --workers 4 --threads 1 --timeout 30 --access-logfile '/home/LogFiles/gunicorn-access.log' --error-logfile '/home/LogFiles/gunicorn-error.log' --bind=0.0.0.0:8000 --chdir=. bamru_net.wsgi
